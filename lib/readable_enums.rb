f4e8675969857a7c0b9628cd92749f878021ddd9
Austin Fisher <ayyfish@outlook.com>
2017-11-15 16:40:33 -0500

    support allow_nil and if validates args

--- lib/readable_enums.rb
+++ lib/readable_enums.rb
@@ -2,14 +2,15 @@ module ReadableEnums
   extend ActiveSupport::Concern
 
   module ClassMethods
+    def readable_enum(name, values, **args)
+      validates_args = args.slice(:allow_nil, :if).compact
 
-    def readable_enum(name, values)
-      validates name.to_sym, inclusion: {in: values, message: "%{value} is not a valid #{name}"}
+      validates name.to_sym, inclusion: { in: values, message: "%{value} is not a valid #{name}" }, **validates_args
 
       values.each do |enum|
         define_method(:"#{enum}?") { send(name.to_s) == enum }
-        define_method(:"#{enum}!") { update(name.to_s => enum)}
-        define_singleton_method(:"#{enum}") { where(name.to_s => enum)}
+        define_method(:"#{enum}!") { update(name.to_s => enum) }
+        define_singleton_method(:"#{enum}") { where(name.to_s => enum) }
       end
     end
   end
